3b6ae56482c17fe2c4a8e334caac22cfb8c688a1
paulo <phmoreira37@gmail.com>
2022-10-30 11:22:37 -0300

    Ver todos os países da API na página principal

--- README.md
+++ README.md
@@ -1,19 +1,19 @@
 # Countries App
 
-Integração com a API REST Countries para pegar dados de países e exibir em uma página. 
+Integração com a API REST Countries para pegar dados de países e exibir em uma página.
 
 Funcionalidades:
- 
-- [ ] Ver todos os países da API na página principal 
-  - [ ] Fazer conexão com api
-  - [ ] Mostrar todos os países na página principal
-    - [ ] Cada país deve conter: bandeira; nome; qtd população; região; capital;
+
+- [x] Ver todos os países da API na página principal 
+  - [x] Mostrar todos os países na página principal
+    - [x] Cada país deve conter: bandeira; nome; qtd população; região; capital;
+  - [x] Criar interface para mostrar todos os países.
 
 - [ ] Procurar por um país atráves de um input
   - [ ] Capturar nome do país
   - [ ] Enviar nome para API
   - [ ] Tratar a resposta e apresentar o resultado em uma página
- 
+
 
 - [ ] Filtro por região
 - [ ] Clicar em um país para obter informações mais detalhadas
---
+++ assets/js/index.js
@@ -0,0 +1,37 @@
+const url = "https://restcountries.com/v3.1/all";
+
+const pageCountries = document.querySelector(".container");
+
+//Pegar todos os países
+async function getAllCountries() {
+  const response = await fetch(url);
+  const allCountries = await response.json();
+
+  allCountries.map((allCountries) => {
+    //criando as tags html que vão receber os países
+    const div = document.createElement("div");
+    const flag = document.createElement("img");
+    const title = document.createElement("h2");
+    const population = document.createElement("p");
+    const region = document.createElement("p");
+    const capital = document.createElement("p");
+
+    flag.src = allCountries.flags.svg;
+    title.innerText = allCountries.name.common;
+    population.innerText = allCountries.population;
+    region.innerText = allCountries.region;
+    capital.innerText = allCountries.capital;
+
+    div.appendChild(flag);
+    div.appendChild(title);
+    div.appendChild(population);
+    div.appendChild(region);
+    div.appendChild(capital);
+
+    pageCountries.appendChild(div);
+  })
+
+  console.log(allCountries);
+}
+
+getAllCountries();
