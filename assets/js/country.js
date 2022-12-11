a2aedfd811dfa56a8ff1e9ed0bd6f671bd3a0131
Paulo Moreira <phmoreira37@gmail.com>
2022-12-11 20:30:33 -0300

    Tratando formato do valor da população

--- assets/js/country.js
+++ assets/js/country.js
@@ -1,10 +1,10 @@
-// Extrai a lógica de fazer a requisição dos "countries" em uma função (princípio da responsabilidade única). 
+// Extrai a lógica de fazer a requisição dos "countries" em uma função (princípio da responsabilidade única).
 async function getCountry(url) {
   try {
     const response = await fetch(url);
     const country = await response.json();
 
-    if (country.constructor.name === "Array") return country[0];
+    if (country.constructor.name === 'Array') return country[0];
 
     return country;
   } catch (error) {
@@ -38,7 +38,7 @@ function countryInfoElementsHandler(country) {
       borders += ` <button class="btn">${element}</button> `;
     });
   } else {
-    borders = "<p>This country have no border countries!</p>";
+    borders = '<p>This country have no border countries!</p>';
   }
 
   return {
@@ -46,38 +46,34 @@ function countryInfoElementsHandler(country) {
     currencies,
     languages,
     borders,
-  }
+  };
 }
 
 function renderCountryDocument(country) {
-  const {
-    topLevelDomain,
-    currencies,
-    languages,
-    borders,
-  } = countryInfoElementsHandler(country);
+  const { topLevelDomain, currencies, languages, borders } =
+    countryInfoElementsHandler(country);
 
-  // Isso aqui talvez fiquei mais dificil de pegar a visão, mas a intenção é deixar dinâmico a geração dos dados do "country". 
+  // Isso aqui talvez fiquei mais dificil de pegar a visão, mas a intenção é deixar dinâmico a geração dos dados do "country".
   // Isso melhora a "manutenibilidade", pois, se eu quiser adicionar/remover algum item, é só modificar o objeto.
   const leftData = [
     {
-      label: "Native Name",
+      label: 'Native Name',
       value: country.nativeName,
     },
     {
-      label: "Population",
-      value: country.population,
+      label: 'Population',
+      value: handlePopulationNumber(country.population),
     },
     {
-      label: "Region",
+      label: 'Region',
       value: country.region,
     },
     {
-      label: "Sub Region",
+      label: 'Sub Region',
       value: country.subregion,
     },
     {
-      label: "Capital",
+      label: 'Capital',
       value: country.capital,
     },
   ];
@@ -85,30 +81,40 @@ function renderCountryDocument(country) {
   // O mesmo aqui.
   const rigthData = [
     {
-      label: "Top Level Domain",
+      label: 'Top Level Domain',
       value: topLevelDomain,
     },
     {
-      label: "Currencies",
+      label: 'Currencies',
       value: currencies,
     },
     {
-      label: "Languages",
+      label: 'Languages',
       value: languages,
     },
   ];
 
   // Note que agora os dados do "country" agora estão dinâmicos. Dessa forma, também evitei repetição de código "html".
-  document.querySelector(".flex-container").innerHTML = `
+  document.querySelector('.flex-container').innerHTML = `
     <img src="${country.flags.svg}" alt="country Image">
     <div class="country-text">
       <h3>${country.name}</h3>
       <div class="info">
         <div class="left">
-          ${leftData.map((detail) => `<p class="details"><span class="strong">${detail.label}: </span> ${detail.value}</p>`).join("")}
+          ${leftData
+            .map(
+              (detail) =>
+                `<p class="details"><span class="strong">${detail.label}: </span> ${detail.value}</p>`
+            )
+            .join('')}
          </div>
          <div class="right">
-          ${rigthData.map((detail) => `<p class="details"><span class="strong">${detail.label}: </span> ${detail.value}</p>`).join("")}
+          ${rigthData
+            .map(
+              (detail) =>
+                `<p class="details"><span class="strong">${detail.label}: </span> ${detail.value}</p>`
+            )
+            .join('')}
          </div>
         </div>
         <div class="border-container">
@@ -121,7 +127,7 @@ function renderCountryDocument(country) {
 }
 
 // Bom, essa função aqui ficou sendo a responsável por lidar com toda a lógica do "country". É a principal desse fluxo.
-// Note que agora, ela consegue lidar tanto com "getCountryByName" e "getCountryByAlphaCode", 
+// Note que agora, ela consegue lidar tanto com "getCountryByName" e "getCountryByAlphaCode",
 // evitando repetição de código (DRY - Dont Repeat Yourself)
 async function countryHandler(url) {
   const country = await getCountry(url);
@@ -130,26 +136,30 @@ async function countryHandler(url) {
   getClickedBorder();
 }
 
-
 function getClickedBorder() {
-  const button = document.querySelectorAll("button");
-  const divContainer = document.querySelector(".flex-container");
+  const button = document.querySelectorAll('button');
+  const divContainer = document.querySelector('.flex-container');
   // Aqui eu internalizei essas variáveis [baseAlphaUrl] que só são usadas neste escopo da função.
-  const baseAlphaUrl = "https://restcountries.com/v2/alpha";
+  const baseAlphaUrl = 'https://restcountries.com/v2/alpha';
 
   button.forEach((element) => {
-    element.addEventListener("click", () => {
-      divContainer.innerHTML = "";
+    element.addEventListener('click', () => {
+      divContainer.innerHTML = '';
       countryHandler(`${baseAlphaUrl}/${element.innerText}`);
     });
   });
 }
 
+// Função para tratar formato do valor da população
+function handlePopulationNumber(population) {
+  return population.toLocaleString('en-US');
+}
+
 (function () {
-  const baseUrl = "https://restcountries.com/v2/name";
+  const baseUrl = 'https://restcountries.com/v2/name';
   // Aqui também eu internalizei essas variáveis [urlSearchParams e countryName] que só são usadas neste escopo da função.
   const urlSearchParams = new URLSearchParams(window.location.search);
-  const countryName = urlSearchParams.get("name");
+  const countryName = urlSearchParams.get('name');
 
   countryHandler(`${baseUrl}/${countryName}?fullText=true`);
 })();
--- assets/js/index.js
+++ assets/js/index.js
@@ -1,45 +1,43 @@
-const url = "https://restcountries.com/v2/all";
+const url = 'https://restcountries.com/v2/all';
 
-const pageCountries = document.querySelector(".container");
+const pageCountries = document.querySelector('.container');
 
 //Pegar todos os países
 async function getAllCountries() {
   const response = await fetch(url);
   const allCountries = await response.json();
 
-  console.log(allCountries)
-
+  console.log(allCountries);
 
   allCountries.map((allCountries) => {
     //criando as tags html que vão receber os países
-    const div = document.createElement("div");
-    const imgDiv = document.createElement("div");
-    const flag = document.createElement("img");
-    const divCountryDetails = document.createElement("div");
-    const title = document.createElement("h2");
-    const population = document.createElement("p");
-    const spanPopulation = document.createElement("span");
-    const region = document.createElement("p");
-    const spanRegion = document.createElement("span");
-    const capital = document.createElement("p");
-    const spanCapital = document.createElement("span");
+    const div = document.createElement('div');
+    const imgDiv = document.createElement('div');
+    const flag = document.createElement('img');
+    const divCountryDetails = document.createElement('div');
+    const title = document.createElement('h2');
+    const population = document.createElement('p');
+    const spanPopulation = document.createElement('span');
+    const region = document.createElement('p');
+    const spanRegion = document.createElement('span');
+    const capital = document.createElement('p');
+    const spanCapital = document.createElement('span');
 
     flag.src = allCountries.flags.svg;
-    flag.setAttribute("alt", `Bandeira do país ${allCountries.name}`);
+    flag.setAttribute('alt', `Bandeira do país ${allCountries.name}`);
     title.innerText = allCountries.name;
-    title.classList.add("title");
-    population.innerText = ("Population: ");
-    spanPopulation.innerText = allCountries.population;
-    region.innerText = ("Region: ");
+    title.classList.add('title');
+    population.innerText = 'Population: ';
+    spanPopulation.innerText = handlePopulationNumber(allCountries.population);
+    region.innerText = 'Region: ';
     spanRegion.innerText = allCountries.region;
-    spanRegion.classList.add("region");
-    capital.innerText = ("Capital: ");
+    spanRegion.classList.add('region');
+    capital.innerText = 'Capital: ';
     spanCapital.innerText = allCountries.capital;
 
-    div.classList.add("country-container");
-    imgDiv.classList.add("image-container");
-    divCountryDetails.classList.add("country-details");
-
+    div.classList.add('country-container');
+    imgDiv.classList.add('image-container');
+    divCountryDetails.classList.add('country-details');
 
     imgDiv.appendChild(flag);
     divCountryDetails.appendChild(title);
@@ -52,83 +50,82 @@ async function getAllCountries() {
     div.appendChild(imgDiv);
     div.appendChild(divCountryDetails);
     pageCountries.appendChild(div);
-  })
+  });
   // Chamando função para pegar país quando for clicado
   getClickedCountry();
 }
 
 //Capturando texto enquanto ele é digitado no input
-const searchInput = document.querySelector("#search-input");
-if(searchInput){
-  searchInput.addEventListener("keyup", () => {
+const searchInput = document.querySelector('#search-input');
+if (searchInput) {
+  searchInput.addEventListener('keyup', () => {
     let text = searchInput.value.toUpperCase();
     valueCompare(text);
-  })
+  });
 }
 
 //Capturando região selecionada no select
-const filterByRegion = document.getElementById("select");
-filterByRegion.addEventListener("change", () => {
+const filterByRegion = document.getElementById('select');
+filterByRegion.addEventListener('change', () => {
   try {
-    const regionValue = filterByRegion.options[filterByRegion.selectedIndex].value;
+    const regionValue =
+      filterByRegion.options[filterByRegion.selectedIndex].value;
     compareRegion(regionValue);
   } catch (error) {
-    console.log('Contornado erro que eu ainda não sei resolver xD')
+    console.log('Contornado erro que eu ainda não sei resolver xD');
   }
-})
+});
 
 //Comparando input com todos os países pegos da API e renderizados na página inicial
 function valueCompare(name) {
-  const countryTitle = pageCountries.getElementsByTagName("h2");
-  const countryContainer = document.querySelectorAll(".country-container");
-  
+  const countryTitle = pageCountries.getElementsByTagName('h2');
+  const countryContainer = document.querySelectorAll('.country-container');
+
   // Fazendo a comparação a cada letra digitada no input
   for (let i = 0; countryTitle.length; i++) {
-    
-    if(countryTitle[i] != null) {
-      const match = countryContainer[i].getElementsByTagName("h2")[0];
-      console.log(match)
+    if (countryTitle[i] != null) {
+      const match = countryContainer[i].getElementsByTagName('h2')[0];
+      console.log(match);
 
-      if(match) {
+      if (match) {
         let countryName = match.textContent || match.innerHTML;
 
-        if(countryName.toUpperCase().indexOf(name) > -1){
-          countryContainer[i].style.display = "";
+        if (countryName.toUpperCase().indexOf(name) > -1) {
+          countryContainer[i].style.display = '';
         } else {
-          countryContainer[i].style.display = "none";
+          countryContainer[i].style.display = 'none';
         }
       }
-
     } else {
-      return
+      return;
     }
   }
 }
 
 //Comparando filtro por região com todos os países pegos da API e renderizados na página inicial
 function compareRegion(region) {
-  const countryRegion = pageCountries.querySelectorAll(".region");
-  const countryContainer = document.querySelectorAll(".country-container");
+  const countryRegion = pageCountries.querySelectorAll('.region');
+  const countryContainer = document.querySelectorAll('.country-container');
 
-  if(!region) {
-    for(let j = 0; countryRegion.length; j++) {
-      countryContainer[j].style.display = "";
+  if (!region) {
+    for (let j = 0; countryRegion.length; j++) {
+      countryContainer[j].style.display = '';
     }
   } else {
     for (let i = 0; countryRegion.length; i++) {
-      if(countryRegion[i] != null) {
-        const match = countryContainer[i].querySelectorAll(".region")[0];
+      if (countryRegion[i] != null) {
+        const match = countryContainer[i].querySelectorAll('.region')[0];
 
-        if(match) {
+        if (match) {
           let regionValue = match.textContent || match.innerHTML;
-          if(regionValue == region) {
-            countryContainer[i].style.display = "";
+          if (regionValue == region) {
+            countryContainer[i].style.display = '';
           } else {
-            countryContainer[i].style.display = "none";
+            countryContainer[i].style.display = 'none';
           }
         }
       } else {
-        return
+        return;
       }
     }
   }
@@ -140,13 +137,18 @@ function changePage(name) {
 }
 
 // Função que pega o país clicado
-function getClickedCountry(){
-  const countryContainer = document.querySelectorAll(".country-container");
-  countryContainer.forEach(element => {
-    element.addEventListener("click", () =>{
+function getClickedCountry() {
+  const countryContainer = document.querySelectorAll('.country-container');
+  countryContainer.forEach((element) => {
+    element.addEventListener('click', () => {
       changePage(element.children[1].children[0].innerHTML);
-    })
-  })
+    });
+  });
+}
+
+// Função para tratar formato do valor da população
+function handlePopulationNumber(population) {
+  return population.toLocaleString('en-US');
 }
 
 // Executar primeiro a função de pegar todos os países da API
